c6724de8dd6fa0dc7d22e0e83f4ff08842c5833b
lavylipesh <lilibethlangat@yahoo.com>
2019-10-08 10:46:40 +0300

    placing email configs in settings.py

--- tribune/settings.py
+++ tribune/settings.py
@@ -161,3 +161,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 django_heroku.settings(locals())
 
+# email configs
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
