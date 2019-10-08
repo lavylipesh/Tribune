3015d3af7585cf75c3d73d2b2ce6dfb0278541fb
lavylipesh <lilibethlangat@yahoo.com>
2019-10-08 10:04:47 +0300

    defining and creating the newsletter form

--- news/views.py
+++ news/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404
 import datetime as dt
 from .models import Article
+from .forms import NewsLetterForm
 
 # Create your views here.
 def welcome(request):
@@ -10,7 +11,14 @@ def welcome(request):
 def news_today(request):
     date = dt.date.today()
     news = Article.todays_news()
-    return render(request,'all-news/today-news.html',{"date":date,"news":news})
+    if request.method == 'POST':
+        form == NewsLetterForm(request.POST)
+        if form.is_valid():
+            print('valid')
+    else:
+        form = NewsLetterForm()
+    
+    return render(request,'all-news/today-news.html',{"date":date,"news":news,"letterForm":form})
 
 def convert_dates(dates):
 
